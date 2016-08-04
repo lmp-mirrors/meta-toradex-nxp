0ca0991f67f34072100685f37909b8db887c2bef
Stefan Agner <stefan.agner@toradex.com>
2016-08-04 11:34:17 -0700

    u-boot-toradex-fw-utils: fix bashism
    Do not use the function keyword, then the script works fine with
    with Busybox' shell implementation.
    
    Signed-off-by: Stefan Agner <stefan.agner@toradex.com>
    Acked-by: Max Krummenacher <max.krummenacher@toradex.com>

--- recipes-bsp/u-boot/files/fw_unlock_mmc.sh
+++ recipes-bsp/u-boot/files/fw_unlock_mmc.sh
@@ -1,5 +1,5 @@
 # Give fw_setenv mmcblk0boot0 write permissions
-function fw_setenv() {
+fw_setenv() {
     echo 0 > /sys/block/mmcblk0boot0/force_ro
     /sbin/fw_setenv "$@"
     echo 1 > /sys/block/mmcblk0boot0/force_ro
