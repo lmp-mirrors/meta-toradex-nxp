lmp-mirrors/meta-toradex-nxp

commit 69bbd0e41af0689f8d6185aae4bf46f6173ddc59

u-boot-toradex: merge .inc file into recipe
The file u-boot-toradex-common_2022.04.inc is only used in
u-boot-toradex_2022.04.bb. Having the information spread over
two files reduces clarity. Additionally our current way of
updating SRCREV with CI/CD is not able to update the SRCREV
in *.inc file if pulled in through relative path.

Related-to: ELB-5266
Signed-off-by: Max Krummenacher <max.krummenacher@toradex.com>

recipes-bsp/u-boot/u-boot-toradex_2022.04.bb
@@ -1,13 +1,47 @@
-require recipes-bsp/u-boot/u-boot.inc
-require recipes-bsp/u-boot/u-boot-toradex-common_${PV}.inc
-
 SUMMARY = "U-Boot bootloader with support for Toradex i.MX 8 series SoMs"
-
-B = "${WORKDIR}/build"
-
+HOMEPAGE = "http://www.denx.de/wiki/U-Boot/WebHome"
+SECTION = "bootloaders"
+LICENSE = "GPL-2.0-or-later"
+LIC_FILES_CHKSUM = "file://Licenses/gpl-2.0.txt;md5=b234ee4d69f5fce4486a80fdaf4a4263"
+DEPENDS = "bc-native dtc-native flex-native bison-native python3-setuptools-native"
 PROVIDES += "u-boot"
-DEPENDS += "bc-native dtc-native python3-setuptools-native"
+
+require recipes-bsp/u-boot/u-boot.inc
+
+SRC_URI = "git://git.toradex.com/u-boot-toradex.git;protocol=https;branch=${SRCBRANCH}"
+SRCREV = "7bd2074193e156358adc5b5065c690371cf78231"
+SRCREV:use-head-next = "${AUTOREV}"
+SRCBRANCH = "toradex_imx_lf_v2022.04"
+
+S = "${WORKDIR}/git"
+B = "${WORKDIR}/build"
 
 BOOT_TOOLS = "imx-boot-tools"
 
-SRCREV = "7bd2074193e156358adc5b5065c690371cf78231"
+inherit toradex-u-boot-localversion
+
+UBOOT_INITIAL_ENV = "u-boot-initial-env"
+
+COMPATIBLE_MACHINE = "(mx8-generic-bsp)"
+PACKAGE_ARCH = "${MACHINE_ARCH}"
+
+do_deploy:append:mx8m-generic-bsp() {
+    # Deploy the u-boot-nodtb.bin and fsl-imx8m*-XX.dtb for mkimage to generate boot binary
+    if [ -n "${UBOOT_CONFIG}" ]
+    then
+        for config in ${UBOOT_MACHINE}; do
+            i=$(expr $i + 1);
+            for type in ${UBOOT_CONFIG}; do
+                j=$(expr $j + 1);
+                if [ $j -eq $i ]
+                then
+                    install -d ${DEPLOYDIR}/${BOOT_TOOLS}
+                    install -m 0777 ${B}/${config}/arch/arm/dts/${UBOOT_DTB_NAME}  ${DEPLOYDIR}/${BOOT_TOOLS}
+                    install -m 0777 ${B}/${config}/u-boot-nodtb.bin  ${DEPLOYDIR}/${BOOT_TOOLS}/u-boot-nodtb.bin-${MACHINE}-${type}
+                fi
+            done
+            unset  j
+        done
+        unset  i
+    fi
+}

recipes-bsp/u-boot/u-boot-toradex-common_2022.04.inc
@@ -1,42 +1,0 @@
-HOMEPAGE = "http://www.denx.de/wiki/U-Boot/WebHome"
-SECTION = "bootloaders"
-DEPENDS += "flex-native bison-native"
-
-LICENSE = "GPL-2.0-or-later"
-LIC_FILES_CHKSUM = "file://Licenses/gpl-2.0.txt;md5=b234ee4d69f5fce4486a80fdaf4a4263"
-
-SRC_URI = "git://git.toradex.com/u-boot-toradex.git;protocol=https;branch=${SRCBRANCH}"
-
-SRCREV = "c71ae7141f30da99e2206a331ac821603f238fa7"
-SRCREV:use-head-next = "${AUTOREV}"
-SRCBRANCH = "toradex_imx_lf_v2022.04"
-
-S = "${WORKDIR}/git"
-
-inherit toradex-u-boot-localversion
-
-UBOOT_INITIAL_ENV = "u-boot-initial-env"
-
-COMPATIBLE_MACHINE = "(mx8-generic-bsp)"
-PACKAGE_ARCH = "${MACHINE_ARCH}"
-
-do_deploy:append:mx8m-generic-bsp() {
-    # Deploy the u-boot-nodtb.bin and fsl-imx8m*-XX.dtb for mkimage to generate boot binary
-    if [ -n "${UBOOT_CONFIG}" ]
-    then
-        for config in ${UBOOT_MACHINE}; do
-            i=$(expr $i + 1);
-            for type in ${UBOOT_CONFIG}; do
-                j=$(expr $j + 1);
-                if [ $j -eq $i ]
-                then
-                    install -d ${DEPLOYDIR}/${BOOT_TOOLS}
-                    install -m 0777 ${B}/${config}/arch/arm/dts/${UBOOT_DTB_NAME}  ${DEPLOYDIR}/${BOOT_TOOLS}
-                    install -m 0777 ${B}/${config}/u-boot-nodtb.bin  ${DEPLOYDIR}/${BOOT_TOOLS}/u-boot-nodtb.bin-${MACHINE}-${type}
-                fi
-            done
-            unset  j
-        done
-        unset  i
-    fi
-}

commit f62e8915300f5f98849e8336eedc991074a1f402

u-boot-toradex: fix provides
PROVIDES is reassigned in the u-boot.inc file. Use `:append`, so that
the value assigned is not overwritten later.

Fixes: 69bbd0e ("u-boot-toradex: merge .inc file into recipe")
| ERROR: Nothing PROVIDES 'u-boot' (but .../imx-boot_1.0.bb DEPENDS on or otherwise requires it)
Related-to: ELB-5266
Signed-off-by: Max Krummenacher <max.krummenacher@toradex.com>

recipes-bsp/u-boot/u-boot-toradex_2022.04.bb
@@ -4,7 +4,7 @@ SECTION = "bootloaders"
 LICENSE = "GPL-2.0-or-later"
 LIC_FILES_CHKSUM = "file://Licenses/gpl-2.0.txt;md5=b234ee4d69f5fce4486a80fdaf4a4263"
 DEPENDS = "bc-native dtc-native flex-native bison-native python3-setuptools-native"
-PROVIDES += "u-boot"
+PROVIDES:append = " u-boot"
 
 require recipes-bsp/u-boot/u-boot.inc
 