lmp-mirrors/meta-toradex-nxp

commit 0bf3fa3a991a0266dcbcf3fd723773b9e6ccf359

linux-toradex: rt: skip upstream status check
The downloaded RT patch doesn't have a upstream status tag.

Signed-off-by: Max Krummenacher <max.krummenacher@toradex.com>

recipes-kernel/linux/linux-toradex_5.15-2.2.x.bb
@@ -48,3 +48,6 @@ SRC_URI:append:preempt-rt = " \
 "
 
 SRC_URI[rt-patch.sha256sum] = "b3494bd8c156550b3bfe27e544928e7ee3f0822d182ed77de54ebef867c67c6d"
+
+# The downloaded RT patch doesn't have a upstream status tag
+INSANE_SKIP:preempt-rt = "patch-status"

commit 4246cbf581d3ee3bde5ff30d6c06d37d430283e4

linux-toradex-ti: rt: skip upstream status check
The downloaded RT patch doesn't have a upstream status tag.
INSANE_SKIP cannot (yet) be used for the `patch-status`.

Fixes: 0bf3fa3a991a ("linux-toradex: rt: skip upstream status check")
Signed-off-by: Max Krummenacher <max.krummenacher@toradex.com>

recipes-kernel/linux/linux-toradex_5.15-2.2.x.bb
@@ -50,4 +50,4 @@ SRC_URI:append:preempt-rt = " \
 SRC_URI[rt-patch.sha256sum] = "b3494bd8c156550b3bfe27e544928e7ee3f0822d182ed77de54ebef867c67c6d"
 
 # The downloaded RT patch doesn't have a upstream status tag
-INSANE_SKIP:preempt-rt = "patch-status"
+ERROR_QA:remove:preempt-rt = "patch-status"