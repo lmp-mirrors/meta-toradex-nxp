lmp-mirrors/meta-toradex-nxp

commit 5dece96bd81618e01fb9c9190b4ec6e72970fc14

imx-sc-firmware: Bump to 1.16.0
This commit bumps version to latest 1.16.0.

As the NXP EULA was also bumped to V56 add the relevant data not
in meta-freescale kirkstone.

Related-to: ELB-5852
Signed-off-by: Hiago De Franco <hiago.franco@toradex.com>
Signed-off-by: Max Krummenacher <max.krummenacher@toradex.com>

recipes-bsp/imx-sc-firmware/imx-sc-firmware-toradex_1.16.0.bb
@@ -4,14 +4,18 @@
 
 DESCRIPTION = "i.MX System Controller Firmware for Toradex hardware"
 LICENSE = "Proprietary"
-LIC_FILES_CHKSUM = "file://COPYING;md5=5a0bf11f745e68024f37b4724a5364fe"
+LIC_FILES_CHKSUM = "file://COPYING;md5=10c0fda810c63b052409b15a5445671a"
+FSL_EULA_FILE_MD5SUM_LA_OPT_NXP_SOFTWARE_LICENSE_V56 = "10c0fda810c63b052409b15a5445671a"
+FSL_EULA_FILE_MD5SUMS_append = " \
+    ${FSL_EULA_FILE_MD5SUM_LA_OPT_NXP_SOFTWARE_LICENSE_V56} \
+"
 SECTION = "BSP"
 
 inherit deploy
 
 SRC_URI = "git://github.com/toradex/i.MX-System-Controller-Firmware.git;branch=master;protocol=https;fsl-eula=true"
 
-SRCREV = "7b7c7f7c834c58e637a53bab281d48f963788e33"
+SRCREV = "bc23c64b45a2d23cfcc2fa5e4951786934db583e"
 SRCREV:use-head-next = "${AUTOREV}"
 
 S = "${WORKDIR}/git"

commit 2a7a388572560a9b388a05047039263ddb8f8c00

imx-sc-firmware: fix override syntax
Fixes: 5dece96bd816 ("imx-sc-firmware: Bump to 1.16.0")
Signed-off-by: Max Krummenacher <max.krummenacher@toradex.com>

recipes-bsp/imx-sc-firmware/imx-sc-firmware-toradex_1.16.0.bb
@@ -6,7 +6,7 @@ DESCRIPTION = "i.MX System Controller Firmware for Toradex hardware"
 LICENSE = "Proprietary"
 LIC_FILES_CHKSUM = "file://COPYING;md5=10c0fda810c63b052409b15a5445671a"
 FSL_EULA_FILE_MD5SUM_LA_OPT_NXP_SOFTWARE_LICENSE_V56 = "10c0fda810c63b052409b15a5445671a"
-FSL_EULA_FILE_MD5SUMS_append = " \
+FSL_EULA_FILE_MD5SUMS:append = " \
     ${FSL_EULA_FILE_MD5SUM_LA_OPT_NXP_SOFTWARE_LICENSE_V56} \
 "
 SECTION = "BSP"