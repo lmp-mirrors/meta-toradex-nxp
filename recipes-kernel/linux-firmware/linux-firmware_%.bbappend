lmp-mirrors/meta-toradex-nxp

commit 16e3fa520440983292f2f05e762a9f61f8a5220a

linux-firmware: add bbappend recipe to bump version to 20250311
Get the latest linux-firmware tag, 20250311. This is a workaround to
solve an unexpected BLE connection timeout issue experienced with the
older firmware, 20240909, the current version of oe-core scarthgap
branch.

The patch to bump the oe-core to 20250311 was already sent to oe-core
mailing list, therefore this recipe should be removed once the patch
is merged upstream.

Related-to: ELB-6037
Signed-off-by: Hiago De Franco <hiago.franco@toradex.com>

recipes-kernel/linux-firmware/linux-firmware_%.bbappend
@@ -1,0 +1,12 @@
+PV = "20250311"
+WHENCE_CHKSUM = "886924eb733c4efcec21dff980795771"
+SRC_URI[sha256sum] = "b1083a36f19aea46f661dcfd4cd462d13933dcb4e7f0dc809525552dd5c3541d"
+
+PACKAGECONFIG[deduplicate] = ",,rdfind-native"
+
+do_install() {
+        sed -i 's:^./check_whence.py:#./check_whence.py:' ${S}/copy-firmware.sh
+
+        oe_runmake 'DESTDIR=${D}' 'FIRMWAREDIR=${nonarch_base_libdir}/firmware' install
+        cp LICEN[CS]E.* WHENCE ${D}${nonarch_base_libdir}/firmware/
+}

commit bc6b6549158d435b58bd9aa3698099b984aeef0e

linux-firmware: fix do_install
The bump to version 20250311 also brought an update to the
copy-firmware.sh script that is used as part of the firmware
installation process. It now supports a "-j" flag for parallel
installation, but only if the parallel package is installed. This is not
the case for us, leading to the following output when do_install is ran:

ERROR: the GNU parallel command is required to use -j

The upstream linux-firmware_20250311.bb deals with this by setting
PARALLEL_MAKE="" so the "-j" flag is not passed. Add this workaround to
our bbappend as well.

Fixes: 16e3fa520440 ("linux-firmware: add bbappend recipe to bump version to 20250311")
Related-to: ELB-6037
Signed-off-by: Ernest Van Hoecke <ernest.vanhoecke@toradex.com>

recipes-kernel/linux-firmware/linux-firmware_%.bbappend
@@ -4,6 +4,9 @@ SRC_URI[sha256sum] = "b1083a36f19aea46f661dcfd4cd462d13933dcb4e7f0dc809525552dd5
 
 PACKAGECONFIG[deduplicate] = ",,rdfind-native"
 
+# Specifying -j requires GNU parallel, which is a part of meta-oe
+PARALLEL_MAKE = ""
+
 do_install() {
         sed -i 's:^./check_whence.py:#./check_whence.py:' ${S}/copy-firmware.sh
 